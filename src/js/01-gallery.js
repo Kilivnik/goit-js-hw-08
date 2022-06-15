4ac5caa74c11311ca81e657225dab814aac0a261
Kilivnik <ekstasy1990@gmail.com>
2022-06-15 22:38:05 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+galleryContainerEl.insertAdjacentHTML(
+  'beforeend',
+  galleryItemsEl(galleryItems)
+);
+function galleryItemsEl(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>`;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
